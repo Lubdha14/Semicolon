c3857f984a9437e9a8083a7389b05b9f7a3102b9
Lubdha14 <110462188+Lubdha14@users.noreply.github.com>
2024-02-12 15:48:23 +0530

    Add Folder

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Cources.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cources() {
+  return (
+    <div>
+      Cource
+    </div>
+  )
+}
+
+export default Cources
